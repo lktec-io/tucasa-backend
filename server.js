33759c6c0fbbba852f719783a55b551f192cb5bc
lktec-io <lktecio@gmail.com>
2025-11-08 21:14:39 +0300

    ---

--- server.js
+++ server.js
@@ -12,13 +12,12 @@ app.use(bodyParser.json());
 
 // create pool
 const pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  password: "Leonard1234#1234",
-  database: "tucasa",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
 });
 
 app.post('/api/register', async (req, res) => {
